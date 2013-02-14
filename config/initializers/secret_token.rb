ee79967ba7f84a49c6eca5af24ea0dd6f19637dc
Carlos Canizal <carlos@codecery.com>
2013-02-13 23:09:21 -0600

    ¨first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoSsh::Application.config.secret_token = '1218ca380db9aaaf594c8975442aaa30b42ef066284a016ecadc856dc8850a69e93d471e4979559feed27452ca66a973309035fd1aca466802464831552adc55'
